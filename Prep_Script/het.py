5a09d035ec70fa5d4f22ec60a9dfb1b68bf762f0
rosehuang24 <61378377+rosehuang24@users.noreply.github.com>
2020-11-22 15:17:03 +0800

    Update het.py

--- Prep_Script/het.py
+++ Prep_Script/het.py
@@ -13,13 +13,14 @@ for lines in inh:
     if not lines.startswith('#'):
         #info = lines.strip().split()[8]
         #print(info)
+        coord=lines.strip().split()
         for line in lines.strip().split()[9:]:
             gt=line.split(":")
             #print(gt[0])
             if gt[0] == "0/1":
-                outh.write(lines)
+                outh.write(coord[0]+"\t"+str(int(coord[1])-1)+"\t"+coord[1]+"\n")
             elif gt[0] == "0|1":
-                outh.write(lines)
+                outh.write(coord[0]+"\t"+str(int(coord[1])-1)+"\t"+coord[1]+"\n")
 
 
 
