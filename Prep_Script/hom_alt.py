de9cc520656bf5ede18a490a6657a7242c0e9fc1
rosehuang24 <61378377+rosehuang24@users.noreply.github.com>
2020-11-22 15:18:17 +0800

    Create hom_alt.py

---
+++ Prep_Script/hom_alt.py
@@ -0,0 +1,28 @@
+import sys
+
+infile = sys.argv[1]
+inh = open(infile, 'r')
+
+outfile = sys.argv[2]
+outh = open(outfile, 'w')
+
+
+
+
+for lines in inh:
+    if not lines.startswith('#'):
+        #info = lines.strip().split()[8]
+        #print(info)
+        coord=lines.strip().split()
+        for line in lines.strip().split()[9:]:
+            gt=line.split(":")
+            #print(gt[0])
+            if gt[0] == "1/1":
+                outh.write(coord[0]+"\t"+str(int(coord[1])-1)+"\t"+coord[1]+"\n")
+            elif gt[0] == "1|1":
+                outh.write(coord[0]+"\t"+str(int(coord[1])-1)+"\t"+coord[1]+"\n")
+
+
+
+outh.close()
+inh.close()
